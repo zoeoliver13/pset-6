c844f6388a4cea719a9423102004845a29e42d1a
Zoe Oliver <ZOliver@ucvts.tec.nj.us>
2020-01-02 10:44:45 -0500

    added items

--- js/app.js
+++ js/app.js
@@ -1,5 +1,28 @@
-window.onload = function() {
-    document.getElementById("text").onclick = input;
+//add to do
+function addItem(){
+    var li = document.createElement("li");
+    var input = document.getElementById("item").value;
+    var add = document.createTextNode(input);
+    li.appendChild(add);
+    if (input === '') {
+      alert("You have to write a to-do!");
+    } else {
+      document.getElementById("list").appendChild(li);
+    }
+      document.getElementById("item").value = "";
+      var span = document.createElement("SPAN");
+//      var text = document.createElement("img");
+  //    text.src = "images/remove.png";
+    //  var src = document.getElementById("remove");
+    //  src.appendChild(text);
+      span.className = "close";
+    //  span.appendChild(text);
+      li.appendChild(span);
 
-const box = function() {
-      let canvas = document.getElementById("firstInput");
+        for (i = 0; i < close.length; i++) {
+          close[i].onclick = function() {
+            var div = this.parentElement;
+            div.style.display = "none";
+          }
+        }
+  }
